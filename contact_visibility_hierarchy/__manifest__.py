2e96f01530f63f0266005468046ff4a4922c98db
Omar Wael <dragontooth304@gmail.com>
2026-07-16 14:26:32 +0300

    Initial commit: added contact visibility module

---
+++ contact_visibility_hierarchy/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Contact Visibility Hierarchy",
+    'version': '1.0',
+    'depends': ['base', 'contacts', 'hr'],
+    'author': "Omar Wael",
+    'category': 'Hidden',
+    'data': [
+        'security/ir_rule.xml'
+    ],
+    'demo': [
+    ],
+}
